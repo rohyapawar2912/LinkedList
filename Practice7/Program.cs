1b126497549429252a86149cef66b17d3dd98d16
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 15:46:04 +0530

    add : Practice7 files

---
+++ Practice7/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Practice7
+{
+  namespace Stack_Q_LL
+  {
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class LLPopFirst
+    {
+        Node head;
+        public void insert(int data)  // 56 30 70
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+            }
+            else
+            {
+                n.next = head;
+                head = n;
+            }
+        }
+
+        public void PopFirst()// 56 30 40 70
+        {
+            Node t = head;
+            if (t == null)
+            { throw new NullReferenceException("list is empty"); }
+            else
+            {
+                head = t.next;
+            }
+        }
+
+        public void display()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        static void Main()
+        {
+            LLPopFirst ll = new LLPopFirst();
+            ll.insert(70);
+            ll.insert(40);
+            ll.insert(30);
+            ll.insert(56);
+            ll.PopFirst(); ll.PopFirst(); ll.PopFirst();
+            ll.display();
+
+        }
+    }
+  }
+
+}
